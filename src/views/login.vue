f45e6c94e5c1ac84a2284cbac4d02264a9eb9e56
gengjiaxiang <gegnjiaxiang@alibaba.com>
2019-06-27 22:42:36 +0800

    校验彻底完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row type="flex" class="row-bg" justify="center" align="middle">
     <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
-      <el-form ref="form" :model="form" label-width="80px" label-position="top" :rules="rules">
+      <el-form ref="loginform" :model="form" label-width="80px" label-position="top" :rules="rules">
         <el-form-item label="用户名" prop="username">
           <el-input v-model="form.username"></el-input>
         </el-form-item>
@@ -10,7 +10,7 @@
         </el-form-item>
 
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button type="primary" @click="onSubmit('loginform')">立即创建</el-button>
           <el-button>取消</el-button>
         </el-form-item>
       </el-form>
@@ -31,15 +31,22 @@ export default {
           { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
         ],
         password: [
-          { required: true, message: "密码", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 3, max: 6, message: "长度在 3 到 6 个字符", trigger: "blur" }
         ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    onSubmit(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log("没问题");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     }
   }
 };
