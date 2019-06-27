3261d77f14788dfcef52e5ea946922da805922bd
gengjiaxiang <gegnjiaxiang@alibaba.com>
2019-06-27 22:01:26 +0800

    初始化和表单校验完成

---
+++ src/views/login.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-row type="flex" class="row-bg" justify="center" align="middle">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form ref="form" :model="form" label-width="80px" label-position="top" :rules="rules">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="form.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "密码", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    }
+  }
+};
+</script>
+<style scoped>
+.row-bg {
+  height: 100%;
+  background-color: #2d434c;
+}
+.el-form {
+  background-color: #fff;
+  padding: 40px 20px;
+  border-radius: 10px;
+}
+</style>
