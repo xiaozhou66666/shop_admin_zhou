6cb2552c70c717991e02135990ada54e9d76289f
gengjiaxiang <gegnjiaxiang@alibaba.com>
2019-06-29 22:22:14 +0800

    user列表完成

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,10 +1,18 @@
 <template>
   <el-table :data="tableData" stripe style="width: 100%">
-    <el-table-column label="姓名" width="180"></el-table-column>
-    <el-table-column label="邮箱" width="180"></el-table-column>
-    <el-table-column label="电话"></el-table-column>
-    <el-table-column label="用户状态"></el-table-column>
-    <el-table-column label="操作"></el-table-column>
+    <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+    <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+    <el-table-column prop="mobile" label="电话"></el-table-column>
+    <el-table-column prop label="用户状态">
+          <template v-slot="{row}">   
+                  <el-switch v-model="row.type" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+         </template>
+    </el-table-column>
+    <el-table-column prop label="操作">
+      <el-button type="primary" plain size="mini" icon="el-icon-edit"></el-button>
+      <el-button type="danger" plain size="mini" icon="el-icon-delete"></el-button>
+                    <el-button type="success" icon="el-icon-check" size="mini" plain>分配角色</el-button></el-button>
+    </el-table-column>
   </el-table>
 </template>
 
@@ -20,13 +28,18 @@ export default {
   created: function() {
     axios({
       url: "http://localhost:8888/api/private/v1/users",
-      data: {
-        query: 1,
+      params: {
         pagenum: 1,
         pagesize: 5
+      },
+      headers: {
+        Authorization: localStorage.getItem("token")
       }
     }).then(res => {
-      console.log(res);
+        console.log(res);
+        
+      this.tableData = res.data.data.users;
+      console.log(this.tableData);
     });
   }
 };
